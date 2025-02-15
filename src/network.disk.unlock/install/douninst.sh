b7ef2ad591b9976a551e55553f1fe1daf4eb8004
Gavin Brown <gav@greycubes.net>
2025-02-15 19:20:06 +0200

    :bug: fix removal of dependancies when removing package

--- src/network.disk.unlock/install/douninst.sh
+++ src/network.disk.unlock/install/douninst.sh
@@ -13,14 +13,15 @@ if [ -L /usr/sbin/cryptsetup ]; then
   fi
 fi
 
+# ToDo: Fix the versioning of clevis and jose so versions are not needed here
 echo "> Removing package clevis"
-if ! /sbin/removepkg clevis-; then
+if ! /sbin/removepkg clevis-20; then
   echo "Failed to remove clevis package, continuing" >&2
   /usr/bin/logger -t "$logger_tag" "Failed to remove clevis package, continuing"
 fi
 
 echo "> Removing package jose"
-if ! /sbin/removepkg jose; then
+if ! /sbin/removepkg jose-12; then
   echo "Failed to remove jose package, continuing" >&2
   /usr/bin/logger -t "$logger_tag" "Failed to remove jose package, continuing"
 fi
