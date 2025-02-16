70b8c7e5948cc92be291575bd2657c2565d33254
Gavin Brown <gav@greycubes.net>
2025-02-16 18:59:08 +0200

    :safety_vest: Reworked setup script to read from raid devices :wrench: Added persistent tang server config setting :necktie: Changed the logic to ask for Tang server before checking for encrypted disks

--- src/network.disk.unlock/usr/local/emhttp/plugins/network.disk.unlock/network.disk.unlock.setup.sh
+++ src/network.disk.unlock/usr/local/emhttp/plugins/network.disk.unlock/network.disk.unlock.setup.sh
@@ -6,13 +6,15 @@ get_encrypted_disks() {
   awk -F'=' '
 /^\[(.*)\]/{section=$1; gsub(/[]["]/,"",section); next}
 /^device=/{device=$2; gsub(/"/,"",device); next}
-/^fsType="luks:/{fsType=$2; gsub(/"/,"",fsType);print section ";" device ";" fsType}
+/^deviceSb=/{deviceSb=$2; gsub(/"/,"",deviceSb); gsub(/^mapper\//,"",deviceSb); next}
+/^fsType="luks:/{fsType=$2; gsub(/"/,"",fsType);print section ";" device ";" fsType ";" deviceSb}
 ' "$input_file"
   ret=$?
   return $ret
 }
 
 disks_file='/usr/local/emhttp/state/disks.ini'
+tang_server_config='/boot/config/plugins/network.disk.unlock/tang_server.cfg'
 
 # Check for root privileges - use effective uid for most accurate result
 if [ "$(id -u)" -ne 0 ]; then
@@ -35,6 +37,53 @@ fi
 # Checking what disks we have and if they are already setup with tang
 
 echo '
+
+████████╗ █████╗ ███╗   ██╗ ██████╗     ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗
+╚══██╔══╝██╔══██╗████╗  ██║██╔════╝     ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
+   ██║   ███████║██╔██╗ ██║██║  ███╗    ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
+   ██║   ██╔══██║██║╚██╗██║██║   ██║    ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
+   ██║   ██║  ██║██║ ╚████║╚██████╔╝    ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+============================================================================================='
+
+# Get & test the tang server from the user
+tang_server=""
+new_tang_server=""
+# Read the tang server from the config file if it exists
+if [ -f "$tang_server_config" ]; then
+  tang_server=$(<"$tang_server_config")
+  echo "Found existing tang server url in config file: $tang_server"
+fi
+
+while true; do
+  if [ -z "$tang_server" ]; then
+    read -r -p 'Enter your tang server url, e.g. http://tang.server:port > ' tang_server
+    # Strip of any trailing /adv from the tang server
+    tang_server=$(sed 's/\/adv$//' <<< "$tang_server")
+    new_tang_server=1
+  fi
+
+  if curl -sf "${tang_server}/adv" >/dev/null; then
+    echo "Successfully connected to tang server at: ${tang_server}"
+    break
+  else
+    echo "Error: Failed to connect to tang server at: [${tang_server}/adv]" >&2
+    echo 'Is the server url correct? Is Tang running? Does the server have the correct firewall rules?' >&2
+    tang_server=""
+  fi
+done
+
+# If the user has entered a new tang server, update the config file
+if [ -n "$new_tang_server" ]; then
+  echo "Updating the config file with the new tang server url: $tang_server"
+  echo "$tang_server" > "$tang_server_config" || {
+    echo "Warning: failed to write tang server url to config file: $tang_server_config, Permissions issue?"
+    echo "Continuing.."
+  }
+fi
+
+
+echo '
 ██╗     ██╗   ██╗██╗  ██╗███████╗    ██████╗ ██╗███████╗██╗  ██╗███████╗
 ██║     ██║   ██║██║ ██╔╝██╔════╝    ██╔══██╗██║██╔════╝██║ ██╔╝██╔════╝
 ██║     ██║   ██║█████╔╝ ███████╗    ██║  ██║██║███████╗█████╔╝ ███████╗
@@ -54,18 +103,34 @@ fi
 
 # Read through the list of encrypted disks, looking for any that don't have tang binds
 echo "Checking the encrypted disks for existing tang server binds:"
-echo "Note: this may take some time if disks are sleeping..."
+echo -e "Note: this may take some time if disks are sleeping...\n"
 devices_to_do=""
 while read -r line; do
-
-  if ! device=$(awk -F';' '{print $2}' 2>/dev/null <<< "$line"); then
+  #line='disk2;sdd;luks:xfs;md2p1'
+  if ! device=$(awk -F';' '{if ($4=="") exit 1; print $4}' 2>/dev/null <<< "$line"); then
     echo "Warning: failed to get device from disk line, was the line formatted correctly? line='$line'" >&2
     continue
   fi
-
-  echo -n "${device}1 : "
-  if ! tang_servers=$(clevis luks list -d "/dev/${device}1" 2>/dev/null ); then
-    echo "Warning: issue readling luks list for device [/dev/${device}1], disk not encrypted?" >&2
+  device="/dev/${device}"
+  if [ ! -b "${device}" ]; then
+    echo "Warning: Unraid configured device [${device}] does not exist, has the device been removed? Skipping device.." >&2
+    continue
+    # echo "Warning: device [${device}] does not exist, trying to find associated raid device" >&2
+    # if ! device=$(awk -F';' '{if ($4=="") exit 1; print $4}' 2>/dev/null <<< "$line"); then
+    #   echo "Warning: failed to get raid device from disk line, was the line formatted correctly, not a raid disk? line='$line'" >&2
+    #   continue
+    # fi
+    # echo "awk ${device}"
+    # device="/dev/${device}"
+    # echo "${device}"
+    # if [ ! -b "${device}" ]; then
+    #   echo "Warning: raid device [${device}] does not exist either, skipping entry" >&2
+    #   continue
+    # fi
+  fi
+  echo -n "${device}: "
+  if ! tang_servers=$(clevis luks list -d "${device}" 2>/dev/null ); then
+    echo "Warning: issue readling luks list for device [${device}], disk not encrypted?" >&2
     continue
   else
     if ! tang_servers=$(echo "$tang_servers" | grep 'tang'); then
@@ -74,7 +139,7 @@ while read -r line; do
       devices_to_do="$device $devices_to_do"
       continue
     else
-      echo -e "already contains at least one tang server bind:\n├─${tang_servers}\n└──skipping\n"
+      echo -e "already contains at least one tang server bind:\n├─ entry ${tang_servers}\n└─ skipping device\n"
       #sed -E "s/^.*/${device}1 slot \0/" <<< "$tang_servers"
       continue
     fi
@@ -86,53 +151,29 @@ done <<< "$luks_disks"
 #devices_to_do='sde;'
 
 if [ -n "$devices_to_do" ]; then
+    echo "You will be prompted to enter your existing disk encryption password for each disk"
+    echo "Please ensure you have your disk encryption password ready before proceeding"
+    echo "Note: after entered the password, the script will pause for a few seconds while the"
+    echo "tang server is setup this is normal, please wait for the script to continue"
+    echo ""
 
-    echo '
-████████╗ █████╗ ███╗   ██╗ ██████╗     ███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗
-╚══██╔══╝██╔══██╗████╗  ██║██╔════╝     ██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗
-   ██║   ███████║██╔██╗ ██║██║  ███╗    ███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝
-   ██║   ██╔══██║██║╚██╗██║██║   ██║    ╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗
-   ██║   ██║  ██║██║ ╚████║╚██████╔╝    ███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║
-   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
-============================================================================================='
-
-    echo "Luks devices found that are missing tang server setup: $devices_to_do"
-    # Get & test the tang server from the user
-    tang_server=""
-    while true; do
-      if [ -z "$tang_server" ]; then
-        read -r -p 'Enter your tang server url, e.g. http://tang.server:port > ' tang_server
-        # Strip of any trailing /adv from the tang server
-        tang_server=$(sed 's/\/adv$//' <<< "$tang_server")
-      fi
-
-      if curl -sf "${tang_server}/adv" >/dev/null; then
-        echo "Successfully connected to tang server at: ${tang_server}"
-        break
-      else
-        echo "Error: Failed to connect to tang server at: [${tang_server}/adv]" >&2
-        echo 'Is the server url correct? Is Tang running? Does the server have the correct firewall rules?' >&2
-        tang_server=""
-      fi
-    done
-
-    echo "Attempting to setup the tang binds....[$devices_to_do]"
+    echo "Setting up the tang binds for devices [$devices_to_do]"
 
     for device in $devices_to_do; do
-        echo "Adding luks tang binding for: '/dev/${device}1' tang \"'{\"url\": \"${tang_server}\"}'\""
-        #  clevis luks bind -d "/dev/${device}1" tang "'{\"url\": \"${tang_server}\"}'"
+        echo "Adding luks tang binding for: '${device}' tang \"'{\"url\": \"${tang_server}\"}'\""
+        #  clevis luks bind -d "${device}" tang "'{\"url\": \"${tang_server}\"}'"
         tang_json="{\"url\": \"${tang_server}\"}"
-        echo -n 'Enter your existing disk encryption password: '
+        echo  'Enter your existing disk encryption password <press enter then wait a few seconds!>: '
         # Connect the stdin from this script to the clevis subscript
         exec 0</dev/tty
-        clevis luks bind -d "/dev/${device}1" -k - tang "$tang_json"
+        clevis luks bind -d "${device}" -k - tang "$tang_json"
         ret=$?
         exec 0<&-
         if [ $ret -ne 0 ] ; then
-            echo "Error: failed to setup tang server for disk: ${device}1, aborting setup procedure" >&2
+            echo "Error: failed to setup tang server for disk: ${device}, aborting setup procedure" >&2
             exit 13
         else
-            echo "Successfully setup tang server for disk: ${device}1"
+            echo "Successfully setup tang server for disk: ${device}"
         fi
     done <<< "$devices_to_do"
 
